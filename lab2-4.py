ae6b8698dbd508ea854ebd6c0a9545efa1973266
p24jmorris <144147868+JonMorris8@users.noreply.github.com>
2023-09-19 13:46:39 -0400

    FInished assinment

---
+++ lab2-4.py
@@ -0,0 +1,17 @@
+a = 1
+b = 2
+c = 3
+x = 2
+y = 5
+first = (3 + 4 * x) / 5
+print("first done")
+print(first)
+sec = 10*(y - 5)*(a + b + c)*(a + b + c) / x
+print("sec done") 
+print(sec)
+last = (9*((4/(x*x))+((9 + x)/(y))))
+print("last done")
+print(last)
+fin = first - sec + last
+print ("finaly")
+print (fin)
